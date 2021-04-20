5456bdcf6483f7c73978ed62e1c93afd2b3b0971
XfaonXE <73316437+XfaonXE@users.noreply.github.com>
2021-04-19 20:50:26 -0400

    Update Router.php

--- src/Router.php
+++ src/Router.php
@@ -1,11 +1,36 @@
 <?php
 
-namespace XENONMC\XPFRAME;
+/**
+ * COPYRIGHT XENONMC 2019 - Current
+ *
+ * XPFRAME and all of its named materials rights belong to XENONMC
+ * You may fork and redistribute materials of this framework as long as proper crediting is given, learn more at https://xenonmc.xyz/resources/XENONMC/XPFRAME/copyright
+ *
+ * @package XENONMC\XPFRAME\cli
+ * @author XENONMC <support@xenonmc.xyz>
+ * @website https://xenonmc.xyz
+ *
+ */
+
+namespace XENONMC\XPFRAME\Router;
 
 class Router {
 
+    /**
+     * get url functions
+     *
+     */
+
+    use \XENONMC\XPFRAME\Router\geturl;
+
+    /**
+     * constructor
+     *
+     */
+
     function __construct()
     {
-        echo 'data';
+        
+        echo parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
     }
 }
